1fb2927c7a9fb1700d3d89b5f7d9f5f9cb588289
AntumDeluge <antumdeluge@gmail.com>
2017-04-15 17:18:42 -0700

    [flowers] Change 'Node is protected' message to 'This area is protected'

--- mods/flowers/init.lua
+++ mods/flowers/init.lua
@@ -317,7 +317,7 @@ local waterlily_def = {
 					itemstack:take_item()
 				end
 			else
-				minetest.chat_send_player(player_name, "Node is protected")
+				minetest.chat_send_player(player_name, "This area is protected")
 				minetest.record_protection_violation(pos, player_name)
 			end
 		end
